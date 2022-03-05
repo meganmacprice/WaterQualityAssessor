2ae1ae2add5032da3872f0bf50c237fa41633257
Megan Maccagnan Price <100741713+meganmacprice@users.noreply.github.com>
2022-03-05 16:21:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
 # In[ ]:
 
 
@@ -14,20 +8,13 @@ from setuptools import setup
 
 setup(
     name='WaterQualityAssessor',
-    version='0.0.2',
+    version='0.1.2',
     author='Megan Maccagnan',
     author_email='meganmaccagnan@mines.edu',
     packages=['WaterQualityAssessor'],
     license='LICENSE.txt',
     url='http://pypi.python.org/pypi/WaterQualityAssessor/',
-    description='Assess the toxicity levels of various trace metals according to EPA MCL standards in post-wildfire habitat watersheds',
+    description='Assess the toxicity levels of various contaminants according to EPA MCL standards in post-wildfire habitat watersheds',
     long_description=open('README.txt').read(),
     install_requires=['pandas','matplotlib','numpy', 'seaborn', 'math'],
     )
-
-
-# In[ ]:
-
-
-
-
